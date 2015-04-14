bbcbef416ea52744b3142f09154034d216655ea6
Matti Pohjanvirta <matti.pohjanvirta@gmail.com>
2015-04-14 10:55:09 +0300

    Implement experience gain for player

--- Assets/creatures/character.cs
+++ Assets/creatures/character.cs
@@ -8,12 +8,30 @@ public class character : baseCreature
     private int hp;
     private int mp;
 
+    public int level = 1;
+    public int experienceTotal = 0;
+    public int experienceToNextLevel;
+
+    private Dictionary<int, int> experienceChart = new Dictionary<int, int>()
+    {
+        {2, 1000},
+        {3, 3000},
+        {4, 7000},
+        {5, 14000},
+        {6, 20000},
+        {7, 27000},
+        {8, 35000},
+        {9, 43000},
+        {10, 53000},
+         
+    };
 
 	// Use this for initialization
 	void Start ()
 	{
 	    hp = baseHp;
 	    mp = baseMp;
+	    experienceChart.TryGetValue(2, out experienceToNextLevel);
 
 	}
 	
@@ -40,5 +58,22 @@ public class character : baseCreature
     public void takeHit(int amount)
     {
         hp = hp - amount;
+    }
+
+    public void earnExp(int amount)
+    {
+        experienceTotal += amount;
+        if (experienceTotal >= experienceToNextLevel)
+        {
+            ding();
+        }
+
+    }
+
+    private void ding()
+    {
+        level++;
+        // tähän pitäisi laskea sit int/agi/str gainin perusteella suurentuneet statsit
+        experienceChart.TryGetValue(level+1, out experienceToNextLevel);
     }
 }
--- Assets/scripts/manager.cs
+++ Assets/scripts/manager.cs
@@ -5,12 +5,13 @@ public class manager : MonoBehaviour
 {
 
     public GameObject player;
-    public GameObject monster;
+    private GameObject monster;
     private creature monsterScript;
+    private character playerScript;
 	// Use this for initialization
 	void Start ()
 	{
-
+	    playerScript = player.GetComponent<character>();
 	}
 	
 	// Update is called once per frame
@@ -20,12 +21,10 @@ public class manager : MonoBehaviour
 
     public void createMonster()
     {
-        if (monster == null)
-        {
-            monster = Instantiate(Resources.Load("monster")) as GameObject;
-            monsterScript = monster.GetComponent<creature>();
-        }
+        if (monster != null) return;
 
+        monster = Instantiate(Resources.Load("monster")) as GameObject;
+        if (monster != null) monsterScript = monster.GetComponent<creature>();
     }
 
     private void killMonster()
@@ -33,6 +32,13 @@ public class manager : MonoBehaviour
         Destroy(monster);
         monsterScript = null;
         monster = null;
+
+        awardXp(500);
+    }
+
+    private void awardXp(int amount)
+    {
+        playerScript.earnExp(amount);
     }
 
     public void hurtPlayer()
